a37f0aa3d6a98e48de236fcc3a914c1088c150da
Nardos Tessema <nardos.tessema@gmail.com>
2025-05-19 22:33:32 -0500

    Constants factored out into a configuraiton file.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import yaml
+
+with open("config.yaml", "r") as config_file:
+    config = yaml.safe_load(config_file)
+
+data_location = config["constants"]["data_location"]
+sql_location = config["constants"]["sql_location"]
 
 # get incident number from user
 incident = input("Enter incident number: ")
@@ -8,7 +15,7 @@ insert = "INSERT INTO table_name (City,Number)\n"
 select = "SELECT '{}','{}' from dual"
 
 # read the CSV file
-data = pd.read_csv("./data/HomeCity.csv")
+data = pd.read_csv(f"./{data_location}/HomeCity.csv")
 
 # iterate over the rows and construct the sql script
 for index, row in data.iterrows():
@@ -17,5 +24,5 @@ for index, row in data.iterrows():
     insert += select.format(row['City'], row['Number'])
 
 # save sql script to file
-with open(f"./sql/{incident}-insert.sql", "w") as file:
+with open(f"./{sql_location}/{incident}-insert.sql", "w") as file:
     file.write(insert)
