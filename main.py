0ba6ff0b3c21020976caeb798d7720e0d61c6f85
Nardos Tessema <nardos.tessema@gmail.com>
2025-05-19 22:42:56 -0500

    Comments updated.

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import yaml
 
+# load configuration params
 with open("config.yaml", "r") as config_file:
     config = yaml.safe_load(config_file)
-
 data_location = config["constants"]["data_location"]
 sql_location = config["constants"]["sql_location"]
 
-# get incident number from user
+# get input from user
 incident = input("Enter incident number: ")
-
-# sql script formats
-insert = "INSERT INTO table_name (City,Number)\n"
-select = "SELECT '{}','{}' from dual"
 
 # read the CSV file
 data = pd.read_csv(f"{data_location}/HomeCity.csv")
+
+# sql select statement template
+select = "SELECT '{}','{}' from dual"
+
+# initialize sql script
+insert = "INSERT INTO table_name (City,Number)\n"
 
 # iterate over the rows and construct the sql script
 for index, row in data.iterrows():
